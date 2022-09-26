c587e143df7efae822c7d5a5d7b990bcfd5780b1
Gabriel Pires <gabpiresdev@gmail.com>
2022-09-26 19:07:15 -0300

    feat: configure stitches to style application and load styles on server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
+import { styled } from '../styles';
 
 const Home: NextPage = () => {
-  return <h1>Hello next</h1>;
+  return (
+    <>
+      <h1>Hello next</h1>
+      <Button>Click</Button>
+    </>
+  );
 };
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 'none',
+  padding: '4px 8px',
+});
 
 export default Home;
