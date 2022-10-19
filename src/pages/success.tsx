c6a2de277b0f7de5a5d136f007a437f53a0ca049
Gabriel Pires <gabpiresdev@gmail.com>
2022-10-19 19:16:52 -0300

    feat: success page styles

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 const Success = () => {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuu! <strong>Gabriel Pires</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href={'/'}>Voltar ao catalogo</Link>
+    </SuccessContainer>
+  );
 };
 
 export default Success;
